13b5683a15eea5a7566f71416ddc1f071bef0c3e
VictorCcorimanya <raulccorimanyaalfaro@gmail.com>
2022-01-05 15:33:51 -0500

    [ADD] mobile menu

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const button = document.querySelector('.menu_btn');
+const mMenu = document.querySelector('.mobile_menu');
 
-
-button.addEventListener('click', function () {
-  if (active) {
-    console.log('Active')
-  } else {
-
-    console.log('Off')
+const showHide = () => {
+  if (mMenu.classList.contains('is-active')){
+    mMenu.classList.remove('is-active')
+  }else{
+    mMenu.classList.add('is-active')
   }
-})
+}
+
+
+button.addEventListener('click', showHide)
 
