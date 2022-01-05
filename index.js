9e831fb6b0f7db76f63e4c53a9deef4f6b1de83c
VictorCcorimanya <raulccorimanyaalfaro@gmail.com>
2022-01-04 19:25:14 -0500

    add nav bar

---
+++ index.js
@@ -0,0 +1,12 @@
+const button = document.querySelector('.menu_btn');
+
+
+button.addEventListener('click', function () {
+  if (active) {
+    console.log('Active')
+  } else {
+
+    console.log('Off')
+  }
+})
+
